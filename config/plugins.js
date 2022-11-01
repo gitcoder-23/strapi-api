cdb27a57dd9c439703baef35993255fa99d06954
prabhat-cn <prabhat@capitalnumbers.com>
2022-11-01 14:06:59 +0530

    📝

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,28 @@
 // cloudinary config
 module.exports = ({ env }) => ({
   // ...
+  "vercel-deploy": {
+    enabled: true,
+    // config: {
+    //   deployHook:
+    //     "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+    //   apiToken: "<vercel-api-token>",
+    //   appFilter: "your-app-name-on-vercel",
+    //   teamFilter: "your-team-id-on-vercel",
+    //   roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    // },
+  },
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {
-          folder: env('CLOUDINARY_FOLDER', ""),
+          folder: env("CLOUDINARY_FOLDER", ""),
         },
         uploadStream: {},
         delete: {},
